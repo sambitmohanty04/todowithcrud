5a1cdbfc390c9cb8dff80571406690b35a8b1945
Sam <sammohanty04@gmail.com>
2023-05-17 23:05:51 +0530

    table sorting

--- src/component/SortableTable.js
+++ src/component/SortableTable.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
 import Table from './Table'
+import { GoArrowDown, GoArrowUp } from "react-icons/go";
 
 const SortableTable = (props) => {
 
     const [sortOrder, setSortOrder] = useState(null)
     const [sortBy, setSortBy] = useState(null)
 
-    const { config } = props;
+    const { config, data } = props;
 
     const handleClick = (label) => {
+
+        if(sortBy && label !== sortBy) {
+            setSortOrder('asc')
+            setSortBy(label)
+            return;
+        }
+
         if (sortOrder === null) {
             setSortOrder('asc');
             setSortBy(label)
         } else if (sortOrder === 'asc') {
-            setSortOrder('des')
+            setSortOrder('desc')
             setSortBy(label)
         } else if (sortOrder === 'desc') {
             setSortOrder(null)
@@ -28,19 +36,66 @@ const SortableTable = (props) => {
 
         return {
             ...column,
-            header: () => <th onClick={() => handleClick(column.label)}>{column.label} is sortable</th>
+            header: () =>
+                <th className='cursor-pointer hover:bg-gray-100' onClick={() => handleClick(column.label)}>
+                    <div className='flex item-center'>
+                        {getIcons(column.label, sortBy, sortOrder)}
+                        {column.label}
+                    </div>
+                </th>
         }
     })
+
+    let sortedData = data;
+
+    if (sortOrder && sortBy) {
+        const { sortValue } = config.find(column => column.label === sortBy)
+
+        sortedData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+            if (typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            } else {
+                return (valueA - valueB) * reverseOrder;
+            }
+        })
+    }
+
     return (
         <>
             <div>
-                {sortOrder} - {sortBy}
+                 
                 <Table {...props}
                     config={updatedConfig}
+                    data={sortedData}
                 />
             </div>
         </>
     )
+}
+
+function getIcons(label, sortBy, sortOrder) {
+    if (label !== sortBy) {
+        return <div>
+            <GoArrowUp />
+            <GoArrowDown />
+        </div>
+    }
+
+    if (sortOrder === null) {
+        return <div>
+            <GoArrowUp />
+            <GoArrowDown />
+        </div>
+    } else if (sortOrder === 'asc') {
+        return <div> <GoArrowUp /> </div>
+    } else if (sortOrder === 'desc') {
+        return <div> <GoArrowDown /> </div>
+    }
 }
 
 export default SortableTable
