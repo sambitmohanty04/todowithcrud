3a8d3b79beafea712644c945a77b8a87e362f39f
Sam <sammohanty04@gmail.com>
2023-05-19 21:07:28 +0530

    reducer starts

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,37 +1,46 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 // import Button from '../component/Button'
 // import useCounter from '../hooks/use-counter'
+
+const reducer = (state, action) => {
+    return
+}
 
 const CounterPage = ({ initialCount }) => {
 
     // const { count, increment } = useCounter(initialCount);
-    const [count, setCount] = useState(initialCount)
-    const [addValue, setAddValue] = useState(0)
+    // const [count, setCount] = useState(initialCount)
+    // const [addValue, setAddValue] = useState(0)
 
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        addValue : 0
+    })
+    console.log(state)
     const increment = () => {
-        setCount(count + 1);
+        // setCount(count + 1);
     }
     const decrement = () => {
-        setCount(count - 1);
+        // setCount(count - 1);
     }
 
     const handleChange = (e) => {
         const value = parseInt(e.target.value) || 0 ;
         // console.log(typeof value)
-        setAddValue(value)
+        // setAddValue(value)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setCount(count + addValue);
+        // setCount(count + addValue);
 
-        setAddValue(0)
+        // setAddValue(0)
     }
 
     return (
         <>
             <div>
-                <h1>Count is {count}</h1>
+                <h1>Count is {state.count}</h1>
                 {/* <Button onClick={increment} type='submit'>Increment</Button>
                 <Button onClick={decrement}>Decrement</Button> */}
                 <div>
@@ -41,7 +50,7 @@ const CounterPage = ({ initialCount }) => {
                 <form className='mt-3' onSubmit={handleSubmit}>
                     <label className='mr-3'>Add a number</label>
                     <input type='number'
-                    value={addValue || ''}
+                    value={state.addValue || ''}
                     onChange={handleChange}
                     className='border-black-500 bg-gray-300 addCounterfotm' />
                     <button type='submit' className='btn btn-primary ml-2'>Add it</button>
