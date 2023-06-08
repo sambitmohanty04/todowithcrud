222f51624a3f6db072aa43b5cc125b3f4ca4be91
Sam <sammohanty04@gmail.com>
2023-06-08 10:36:06 +0530

    first update

--- src/store/index.js
+++ src/store/index.js
@@ -36,10 +36,9 @@ const store = configureStore({
 const startingState = store.getState();
 console.log(JSON.stringify(startingState))
 
-store.dispatch({
-    type : 'song/addSong',
-    payload : 'New Song'
-})
+store.dispatch(
+    songsSlice.actions.addSong('some song')
+)
 
 const finalState = store.getState()
 console.log(JSON.stringify(finalState))
